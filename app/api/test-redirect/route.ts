59566b2e4b782305f55b3ca92b57ac84de56e63b
incogkahuna <danielhorgan24@gmail.com>
2025-10-07 21:32:45 -0700

    Clean up test routes causing deployment issues - remove test-redirect API and test/callback page

--- app/api/test-redirect/route.ts
+++
@@ -1,9 +0,0 @@
-   import { NextRequest, NextResponse } from 'next/server'
-
-   export async function GET(request: NextRequest) {
-     console.log('🧪 TEST REDIRECT ROUTE CALLED!')
-     console.log('Request URL:', request.url)
-     
-     // Simple redirect to Google to test if redirects work at all
-     return NextResponse.redirect('https://www.google.com')
-   }
